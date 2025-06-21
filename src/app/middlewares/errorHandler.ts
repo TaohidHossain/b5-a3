2ca87ba82180256fc8475b0d646c9a5933ced41c
TaohidHossain <htaohid31@gmail.com>
2025-06-21 11:14:57 +0600

    chore: add zod error handler

--- src/app/middlewares/errorHandler.ts
+++ src/app/middlewares/errorHandler.ts
@@ -1,6 +1,7 @@
-import { ErrorRequestHandler, Response } from "express";
-import { INTERNAL_SERVER_ERROR } from "../constants/httpStatusCodes";
+import e, { ErrorRequestHandler, Response } from "express";
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from "../constants/httpStatusCodes";
 import AppError from "../utils/AppError";
+import * as z from "zod/v4";
 
 
 function handelAppError(res: Response ,err: AppError) {
@@ -12,10 +13,27 @@ function handelAppError(res: Response ,err: AppError) {
     })
 }
 
+function handleZodError(res: Response, err: z.ZodError) {
+    const errors = err.issues.map(err => ({
+        path: err.path.join("."),
+        message: err.message
+    }));
+    return res.status(BAD_REQUEST).json({
+        message: "Validation Failed",
+        success: false,
+        error: {
+            name: err.name,
+            errors,
+        },
+    })
+}
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
     if(err instanceof AppError) {
         handelAppError(res, err);
+    }
+    else if(err instanceof z.ZodError) {
+        handleZodError(res, err);
     }
     else {
         res.status(INTERNAL_SERVER_ERROR).json({
