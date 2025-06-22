09ab33b606d3acb8b89d425b869d5108618bb953
TaohidHossain <htaohid31@gmail.com>
2025-06-22 11:51:38 +0600

    feat: add global not found handler

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import errorHandler from './app/middlewares/errorHandler';
-import appAssert from './app/utils/appAssert';
-import { INTERNAL_SERVER_ERROR } from './app/constants/httpStatusCodes';
+import { NOT_FOUND } from './app/constants/httpStatusCodes';
 import router from './app/routes';
 
 const app: Express = express();
@@ -20,5 +19,18 @@ app.get('/', (req: Request, res: Response) => {
 
 app.use("/api", router)
 
+// Global error handler
 app.use(errorHandler);
+// Handle 404 errors
+app.use(/(.*)/, (req: Request, res: Response) => {
+    res.status(NOT_FOUND).json({
+      message: "Not a valid api end point",
+      success: false,
+      error: {
+        name: "NotFoundError",
+        message: "The requested resource was not found"
+      }
+    })
+})
+
 export default app;
