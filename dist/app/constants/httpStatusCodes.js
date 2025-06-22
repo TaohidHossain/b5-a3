05cb94097e1e77594ec5f003e89df43cd33793c7
TaohidHossain <htaohid31@gmail.com>
2025-06-22 12:37:40 +0600

    chore: change outDir from dist to api

--- dist/app/constants/httpStatusCodes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.INTERNAL_SERVER_ERROR = exports.NOT_FOUND = exports.BAD_REQUEST = exports.NO_CONTENT = exports.CREATED = exports.OK = void 0;
-exports.OK = 200;
-exports.CREATED = 201;
-exports.NO_CONTENT = 204;
-exports.BAD_REQUEST = 400;
-exports.NOT_FOUND = 404;
-exports.INTERNAL_SERVER_ERROR = 500;
--- dist/app/modules/book/book.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const book_controller_1 = require("./book.controller");
-const router = (0, express_1.Router)();
-router.route("/").post(book_controller_1.createBook).get(book_controller_1.getAllBooks);
-router.route("/:bookId").get(book_controller_1.getBookById).put(book_controller_1.updateBook).delete(book_controller_1.deleteBook);
-exports.default = router;
